44d4b38ec6aa6dfd7c630b43a538301fff30add6
Amanda <amanda@ciobanu.org>
2020-06-17 13:44:06 +0100

    fixed typos mistakes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :categories, inclusion: { in: %w(Chinese Italian Japanese French Belgian),
-             message: "%{value} is not a valid category" }
+  #validates :categories, inclusion: { in: %w(Chinese Italian Japanese French Belgian),
+  #           message: "%{value} is not a valid category" }
 end
